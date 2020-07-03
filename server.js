e5afffbc27a73108394a205932f775095c41ee25
Derek Stutts <dstutts@redventures.com>
2020-07-03 17:27:10 -0400

    removes unneeded port assignment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const API = require('call-of-duty-api')(({platform: 'psn'}));
-const port = 5500;
 
 app.use(cors());
 
